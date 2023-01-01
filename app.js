14dbeea52ea31502a05d2127d28a39d6cf1b5f41
hichemhf <hichemhf3@gmail.com>
2023-01-01 18:32:04 +0100

    groupe dataChunks of file with progress working

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const fileToDownloaded = "assets.zip";
+const fileToDownload = "assets.zip";
 const startDownloadElem = document.getElementById("startDownload");
+const downloadProgressElem = document.querySelector(
+  ".download-progress-bar__progress"
+);
 
 startDownloadElem.addEventListener("click", () => {
   console.log("Download Started");
-
-  fetch(`/${fileToDownloaded}`)
+  startDownloadElem.setAttribute("disabled", "true");
+  const dataChunks = [];
+  fetch(`/${fileToDownload}`)
     .then((response) => {
       const reader = response.body.getReader();
       const totalSize = Number(response.headers.get("content-length"));
@@ -16,11 +20,14 @@ startDownloadElem.addEventListener("click", () => {
           //result.done
           //result.value
           if (result.value) {
+            dataChunks.push(result.value);
             totalSizeDownloaded += result.value.length;
             const percentage = Math.floor(
               (totalSizeDownloaded / totalSize) * 100
             );
             console.log(`${totalSizeDownloaded}/${totalSize} (${percentage}%)`);
+            downloadProgressElem.style.width = `${percentage}%`;
+            downloadProgressElem.textContent = `${percentage}%`;
           }
 
           if (!result.done) {
@@ -31,6 +38,22 @@ startDownloadElem.addEventListener("click", () => {
 
       return readData();
     })
-    .catch()
-    .finally();
+    .then(() => {
+      console.log("download finshed");
+      const downloadAnchor = document.createElement("a");
+      const blob = new Blob(dataChunks);
+      downloadAnchor.href = URL.createObjectURL(blob);
+      downloadAnchor.download = fileToDownload;
+      document.body.appendChild(downloadAnchor);
+      downloadAnchor.click();
+      document.body.removeChild(downloadAnchor);
+    })
+    .catch((error) => {
+      downloadProgressElem.textContent = "Download error";
+      downloadProgressElem.classList.add("error");
+      console.error(error);
+    })
+    .finally(() => {
+      startDownloadElem.removeAttribute("disabled");
+    });
 });
